7e7158325b27b16623e276196aa5cd9cb4760b89
siamass <102547926+siamass@users.noreply.github.com>
2022-05-04 17:55:38 +0300

    Add files via upload

---
+++ code-v0.2/DoctorLogUsername.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sxediasilogismikoy
+{
+    class DoctorLogUsername
+    {
+        static string DoctorUsername;
+        public static string doctorUsername
+        {
+            get
+            {
+                return DoctorUsername;
+            }
+            set
+            {
+                DoctorUsername = value;
+            }
+        }
+    }
+}
---
+++ code-v0.2/UserLogAMKA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sxediasilogismikoy
+{
+    class UserLogAMKA
+    {
+        static string UserAMKA;
+        public static string userAMKA 
+        {
+            get
+            {
+                return UserAMKA;
+            }
+            set
+            {
+                UserAMKA = value;
+            }
+        }
+    }
+}
